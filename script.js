3fcfe2d22b0ffb6dae31a284f32d00785d4f622f
nngu0157 <nngu0157@student.monash.edu>
2026-04-04 19:23:36 +1100

    Support WebSocket reconnection on request

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 window.onload = function () {
 	var conn;
 	var log = document.getElementById("log");
-	var msg = document.getElementById("msg");
 
 	function appendLog(item) {
 		var doScroll = log.scrollTop === log.scrollHeight - log.clientHeight;
@@ -11,15 +10,22 @@ window.onload = function () {
 		}
 	}
 
-	conn = new WebSocket("ws://10.0.0.1/ws");
+	document.getElementById("clear").onclick = function (evt) {
+		log.textContent = "";
+	};
+
 	if (window["WebSocket"]) {
-		if (conn) {
+		document.getElementById("connect").onclick = function (evt) {
+			document.getElementById("connect").disabled = true;
+			document.getElementById("status").innerHTML = "Connecting...";
+			conn = new WebSocket("ws://10.0.0.1/ws");
+
 			conn.onopen = function (evt) {
-				document.getElementById("sendMsg").disabled = false
-				document.getElementById("status").innerHTML = "Connection opened"
+				document.getElementById("status").innerHTML = "Connection opened.";
 			}
 			conn.onclose = function (evt) {
-				document.getElementById("status").innerHTML = "Connection closed"
+				document.getElementById("connect").disabled = false;
+				document.getElementById("status").innerHTML = "Connection closed or failed.";
 			};
 			conn.onmessage = function (evt) {
 				var messages = evt.data.split('\n');
@@ -28,26 +34,12 @@ window.onload = function () {
 					item.innerText = messages[i];
 					appendLog(item);
 				}
-			}
-		}
+			};
+		};
 	} else {
 		var item = document.createElement("pre");
 		item.innerHTML = "<b>Your browser does not support WebSockets.</b>";
 		appendLog(item);
+		document.getElementById("connect").disabled = true;
 	}
-
-	document.getElementById("form").onsubmit = function () {
-		if (!conn) {
-			return false;
-		}
-		if (!msg.value) {
-			return false;
-		}
-		conn.send(msg.value);
-		var item = document.createElement("pre");
-		item.classList.add("subscribeMsg");
-		item.innerHTML = msg.value;
-		appendLog(item);
-		return false;
-	};
 };
