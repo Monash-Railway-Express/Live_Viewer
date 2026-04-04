10d745b9a6cd503a1c9bc52213f4e72d4ad319ee
nngu0157 <nngu0157@student.monash.edu>
2026-04-04 21:43:14 +1100

    Add service worker to cache

---
+++ sw.js
@@ -0,0 +1,43 @@
+const cacheName = "cache";
+
+// From https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers
+const addResourcesToCache = async (resources) => {
+	const cache = await caches.open(cacheName);
+	await cache.addAll(resources);
+};
+
+self.addEventListener("install", (event) => {
+	event.waitUntil(
+		addResourcesToCache([
+			"/",
+			"/index.html",
+			"/style.css",
+			"/script.js",
+			"/translate_row.js",
+			"/sheet.json",
+			"/spec.json",
+			"/decode_bytes.js",
+		]),
+	);
+});
+
+// From https://developer.chrome.com/docs/workbox/caching-strategies-overview#network_first_falling_back_to_cache
+self.addEventListener("fetch", (event) => {
+	// Check if this is a navigation request
+	if (event.request.mode === "navigate") {
+		// Open the cache
+		event.respondWith(caches.open(cacheName).then((cache) => {
+			// Go to the network first
+			return fetch(event.request.url).then((fetchedResponse) => {
+				cache.put(event.request, fetchedResponse.clone());
+
+				return fetchedResponse;
+			}).catch(() => {
+				// If the network is unavailable, get
+				return cache.match(event.request.url);
+			});
+		}));
+	} else {
+		return;
+	}
+});
